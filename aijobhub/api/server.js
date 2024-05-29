46d8e9a2ae871c0fb8c9e569ede4b50ff9d64164
Cronzoe <redcap643@gmail.com>
2024-05-30 02:12:13 +0500

    created further API's and worked UI.

--- aijobhub/api/server.js
+++ aijobhub/api/server.js
@@ -1,5 +1,13 @@
 import express from "express";
 import mongoose from "mongoose"
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
 
 const app = express()
 const password = encodeURIComponent('Mongo@123');
@@ -14,6 +22,18 @@ try {
   } catch (error) {
     console.log(error);
   }
+  
+  app.use(express.json());
+
+  app.use("/api/user" , userroute)
+  app.use("/api/auth", authRoute);
+  app.use("/api/users", userRoute);
+  app.use("/api/gigs", gigRoute);
+  app.use("/api/orders", orderRoute);
+  app.use("/api/conversations", conversationRoute);
+  app.use("/api/messages", messageRoute);
+  app.use("/api/reviews", reviewRoute);
+
 
 app.listen(8800,()=> {
 console.log("Backend server is running!")
